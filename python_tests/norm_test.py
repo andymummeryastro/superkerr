7d6444329956d91562912d171a87d1fdd7e0e464
Andrew Mummery <andrew.mummery@physics.ox.ac.uk>
2023-08-22 09:33:47 +0100

    Temperature scale test added, now all tests grouped into directory python_tests. Added annoying mac file structureing to gitignore.

--- python_tests/norm_test.py
+++ python_tests/norm_test.py
@@ -10,5 +10,5 @@ h = constants.h
 rg = G*Ms/c**2
 pi = 3.14159265359
 
-norm = sigma_sb / (pi * k**4) * (rg/(units.kpc))**2 * (k * units.Kelvin)**4 * 1/units.keV * (units.keV/(units.Kelvin * k))**4
+norm = sigma_sb / (pi * k**4) * (rg/(units.kpc))**2 * (units.keV)**4 * 1/units.keV 
 print(norm.to(1/units.cm**2 * 1/units.s))
